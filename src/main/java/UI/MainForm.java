9d8c86ae70d08ae3c973310133dc47b93310eec8
Yanick Ouellet <yanickouellet555@gmail.com>
2015-11-04 10:18:47 -0500

    Add demo repaint for the mapPanel

--- src/main/java/UI/MainForm.java
+++ src/main/java/UI/MainForm.java
@@ -1,17 +1,28 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm {
     private JPanel contentPane;
     private JPanel mapPane;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("MainForm");
+        final JFrame frame = new JFrame("MainForm");
         frame.setContentPane(new MainForm().contentPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        Timer t = new Timer(16, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.repaint();
+            }
+        });
+
+        t.start();
     }
 
     private void createUIComponents() {
--- src/main/java/UI/MapPanel.java
+++ src/main/java/UI/MapPanel.java
@@ -4,11 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MapPanel extends JPanel {
+    private int x;
+
+    public MapPanel() {
+        x = 0;
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.WHITE);
-        g.drawString("Hello world!", 25, 25);
+        g.setColor(x % 8 == 0 ? Color.WHITE : Color.cyan);
+        g.drawString("Hello world!", x, 25);
+        x = x > getWidth() - 25 ? 0 : x + 1;
     }
 }
